94e9caf2d0e1de179c7ca16cfb4f67c42f414b08
aaleexcaazaallaa <acazper592@g.educaand.es>
2023-03-21 11:27:01 +0100

    Interfaz subtitulos por Alejandro Cazalla

---
+++ src/main/java/streaming/Subtitulos.java
@@ -0,0 +1,16 @@
+package streaming;
+
+public interface Subtitulos {
+	public String[] idiomas = {
+		"Espanol",
+	    "Ingles",
+	    "Aleman",
+	    "Portugues",
+	    "Frances",
+	    "Japones",
+	    "Chino"
+	};
+	   
+	public void subtitulosPermitidos();
+	    
+}
