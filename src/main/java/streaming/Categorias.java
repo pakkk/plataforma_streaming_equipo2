455401bf0da8fe876e19af2a9b370c18e750ce08
1DAM <1DAM@1DAM-13>
2023-03-21 11:36:08 +0100

    Subida Clase Categorias JavierM

--- src/main/java/streaming/Categorias.java
+++ src/main/java/streaming/Categorias.java
@@ -1,5 +1,35 @@
 package streaming;
 
 public class Categorias {
+	private String nombre;
+    private String descripcion;
 
+    public Categorias(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + " Nombre = " + nombre + ""
+        		+ "\nDescripcion = " + descripcion + '}';
+    }
+    
+    
 }
