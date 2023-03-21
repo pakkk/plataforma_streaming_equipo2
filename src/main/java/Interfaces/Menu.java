d0ff11ec64bca4af75f06a22a0211d1d8b7f3b67
Javier <jmarmeg1005@g.educaand.es>
2023-03-21 17:36:17 +0100

    Actualizacion Menu JavierM

--- src/main/java/Interfaces/Menu.java
+++ src/main/java/Interfaces/Menu.java
@@ -1,5 +1,13 @@
 package Interfaces;
 
 public interface Menu {
+	
+		public void altas(Object nuevo);
+		
+		public void bajas(int posicion);
+		
+		public void modificadiones(int posicion);
+		
+		public void busquedas(String busqueda);
 		
 }
