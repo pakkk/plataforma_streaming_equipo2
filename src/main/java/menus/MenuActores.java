1fbf3ba5e2a3dbd60f5eb7929c8d3cd0af141a5e
Alejandro Cazalla Pérez <acazper592@h.educaand.es>
2023-03-23 18:34:54 +0100

    TAREA 010 - Menu de actores finalizado

--- src/main/java/menus/MenuActores.java
+++ src/main/java/menus/MenuActores.java
@@ -12,11 +12,16 @@ import java.util.Arrays;
  *
  * @author alexc
  */
+
+//Clase que se encarga de almacenar los metodos de altas, bajas, consultas y busquedas de Actores
 public class MenuActores{
     //Array de actores
     private Actores act[] = new Actores[0];
     
-    //Metodo para dar de alta a los actores
+    /* Se pide un Array de objetos Actores
+    * Se añade un Actor al Array
+    * Se devuelve el Array 
+    */
     public Object[] AltasActores(Object[] otro, String nombre, String apellidos, Sexo gender, int edad, String premios)
     {
         otro = (Actores []) act;
@@ -25,7 +30,10 @@ public class MenuActores{
         return otro;
     }
     
-    //Metodo donde se da de baja a los actores haciendo una busqueda por nombres
+    /* Se pide un Array de objetos Actores y el nombre del actor que se va a dar de baja
+    * Se elimina un Actor del Array
+    * Se devuelve el Array 
+    */
     public Object[] BajasActoresPorNombre(Object[] otro, String nombre)
     {
         otro = (Actores []) act;
@@ -41,6 +49,86 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores y el apellido del actor que se va a dar de baja
+    * Se elimina un Actor del Array
+    * Se devuelve el Array 
+    */
+    public Object[] BajasActoresPorApellidos(Object[] otro, String apellidos)
+    {
+        otro = (Actores []) act;
+        for(int i = 0; i < otro.length; i++)
+        {
+            if(apellidos.equalsIgnoreCase(act[i].getApellidos()))
+            {   
+                System.out.println("Actores dados de baja: " + otro[i]);
+                System.arraycopy (otro, i+1, otro, i, otro.length-i-1);
+                otro = Arrays.copyOf (otro, otro.length-1) ;
+            }
+        }
+        return otro;
+    }
+    
+    /* Se pide un Array de objetos Actores y el genero del actor que se va a dar de baja
+    * Se elimina un Actor del Array
+    * Se devuelve el Array 
+    */
+    public Object[] BajasActoresPorGenero(Object[] otro, Sexo gender)
+    {
+        otro = (Actores []) act;
+        for(int i = 0; i < otro.length; i++)
+        {
+            if(gender.equals(act[i].getGender()))
+            {   
+                System.out.println("Actores dados de baja: " + otro[i]);
+                System.arraycopy (otro, i+1, otro, i, otro.length-i-1);
+                otro = Arrays.copyOf (otro, otro.length-1) ;
+            }
+        }
+        return otro;
+    }
+    
+    /* Se pide un Array de objetos Actores y la edad del actor que se va a dar de baja
+    * Se elimina un Actor del Array
+    * Se devuelve el Array 
+    */
+    public Object[] BajasActoresPorEdad(Object[] otro, int edad)
+    {
+        otro = (Actores []) act;
+        for(int i = 0; i < otro.length; i++)
+        {
+            if(edad == act[i].getEdad())
+            {   
+                System.out.println("Actores dados de baja: " + otro[i]);
+                System.arraycopy (otro, i+1, otro, i, otro.length-i-1);
+                otro = Arrays.copyOf (otro, otro.length-1) ;
+            }
+        }
+        return otro;
+    }
+    
+    /* Se pide un Array de objetos Actores y los premios del actor que se va a dar de baja
+    * Se elimina un Actor del Array
+    * Se devuelve el Array 
+    */
+    public Object[] BajasActoresPorPremios(Object[] otro, String premios)
+    {
+        otro = (Actores []) act;
+        for(int i = 0; i < otro.length; i++)
+        {
+            if(premios.equalsIgnoreCase(act[i].getPremios()))
+            {   
+                System.out.println("Actores dados de baja: " + otro[i]);
+                System.arraycopy (otro, i+1, otro, i, otro.length-i-1);
+                otro = Arrays.copyOf (otro, otro.length-1) ;
+            }
+        }
+        return otro;
+    }
+    
+    /* Se pide un Array de objetos Actores,  el nombre del actor que se va a buscar y su sustitucion
+    * Se modifica los objetos Actores seleccionados
+    * Se devuelve el Array 
+    */
     public Object[] ModificacionActoresPorNombre(Object[] otro, String nombre, String nuevoNombre)
     {
         otro = (Actores []) act;
@@ -56,6 +144,10 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores,  los apellidos del actor que se va a buscar y su sustitucion
+    * Se modifica los objetos Actores seleccionados
+    * Se devuelve el Array 
+    */
     public Object[] ModificacionActoresPorApellidos(Object[] otro, String apellidos, String nuevosApellidos)
     {
         otro = (Actores []) act;
@@ -71,6 +163,10 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores,  el genero del actor que se va a buscar y su sustitucion
+    * Se modifica los objetos Actores seleccionados
+    * Se devuelve el Array 
+    */
     public Object[] ModificacionActoresPorGenero(Object[] otro, Sexo gender, Sexo nuevoGender)
     {
         otro = (Actores []) act;
@@ -86,6 +182,10 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores,  la edad del actor que se va a buscar y su sustitucion
+    * Se modifica los objetos Actores seleccionados
+    * Se devuelve el Array 
+    */
     public Object[] ModificacionActoresPorEdad(Object[] otro, int edad, int nuevaEdad)
     {
         otro = (Actores []) act;
@@ -101,6 +201,10 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores,  los premios del actor que se va a buscar y su sustitucion
+    * Se modifica los objetos Actores seleccionados
+    * Se devuelve el Array 
+    */
     public Object[] ModificacionActoresPorPremiois(Object[] otro, String premios, String nuevosPremios)
     {
         otro = (Actores []) act;
@@ -116,6 +220,10 @@ public class MenuActores{
         return otro;
     }
     
+    /* Se pide un Array de objetos Actores y el nombre del actor que se va a buscar
+    * Se buscan los objetos Actores
+    * Se devuelve el Array 
+    */
     public void BusquedaActoresPorNombre(Object[] otro, String nombre)
     {
         otro = (Actores []) act;
@@ -135,6 +243,10 @@ public class MenuActores{
         }
     }
     
+    /* Se pide un Array de objetos Actores y los apellidos del actor que se va a buscar
+    * Se buscan los objetos Actores
+    * Se devuelve el Array 
+    */
     public void BusquedaActoresPorApellidos(Object[] otro, String apellidos)
     {
         otro = (Actores []) act;
@@ -154,6 +266,10 @@ public class MenuActores{
         }
     }
     
+    /* Se pide un Array de objetos Actores y el genero del actor que se va a buscar
+    * Se buscan los objetos Actores
+    * Se devuelve el Array 
+    */
     public void BusquedaActoresPorGenero(Object[] otro, Sexo gender)
     {
         otro = (Actores []) act;
@@ -173,6 +289,10 @@ public class MenuActores{
         }
     }
     
+    /* Se pide un Array de objetos Actores y la edad del actor que se va a buscar
+    * Se buscan los objetos Actores
+    * Se devuelve el Array 
+    */
     public void BusquedaActoresPorEdad(Object[] otro, int edad)
     {
         otro = (Actores []) act;
@@ -192,6 +312,10 @@ public class MenuActores{
         }
     }
     
+    /* Se pide un Array de objetos Actores y ls premios del actor que se va a buscar
+    * Se buscan los objetos Actores
+    * Se devuelve el Array 
+    */
     public void BusquedaActoresPorPremios(Object[] otro, String premios)
     {
         otro = (Actores []) act;
