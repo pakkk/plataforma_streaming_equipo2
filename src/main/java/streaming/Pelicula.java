781cb05762253eaf9853d466beb2d0508858b0c9
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-23 20:50:15 +0100

    Tarea008 Introduccion de comentarios

--- src/main/java/streaming/Pelicula.java
+++ src/main/java/streaming/Pelicula.java
@@ -1,11 +1,15 @@
 package streaming;
 
 public class Pelicula extends Audiovisual {
-
+	//Clase que se encarga de almacenar los datos de audiovisual para despues cargarlo en un array
+	
+	//Constructor
+	//Este constructor proviene de la clase Audiovisual 
 	public Pelicula(String titulo, String idiomaOriginal, int duracion, int vecesVisto) {
 		super(titulo, idiomaOriginal, duracion, vecesVisto);
-		// TODO Auto-generated constructor stub
-	}
 
+	}
+	//Los metodos vienen heredados de la clase Audiovisual por lo que no hace falta introducirlos aqui
+	
 	
 }
