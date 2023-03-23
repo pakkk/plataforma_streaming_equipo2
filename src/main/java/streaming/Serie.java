f773a5c14e08440e0853011c5523a690bbf37da9
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-23 20:49:49 +0100

    Tarea007 Introduccion de comentarios

--- src/main/java/streaming/Serie.java
+++ src/main/java/streaming/Serie.java
@@ -1,11 +1,14 @@
 package streaming;
 
 public class Serie extends Audiovisual{
-
+	//Clase que se encarga de almacenar los datos de audiovisual para despues cargarlo en un array
+	
+	//Constructor
+	//Este constructor proviene de la clase Audiovisual 
 	public Serie(String titulo, String idiomaOriginal, int duracion, int vecesVisto) {
 		super(titulo, idiomaOriginal, duracion, vecesVisto);
-		// TODO Auto-generated constructor stub
 	}
+	//Los metodos vienen heredados de la clase Audiovisual por lo que no hace falta introducirlos aqui
 
 	
 
