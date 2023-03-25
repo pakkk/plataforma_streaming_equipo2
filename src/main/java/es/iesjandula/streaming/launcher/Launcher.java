ae2cf26fbefc0dc02a67f392c29295ee1965d06d
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-25 23:16:02 +0100

    Se añade la aplicacion de las bajas al launcher

--- src/main/java/es/iesjandula/streaming/launcher/Launcher.java
+++ src/main/java/es/iesjandula/streaming/launcher/Launcher.java
@@ -192,6 +192,282 @@ public class Launcher{
 			switch(option)
 			{
 				case "1":
+				//In this case the user enter to a better menu that the last were he can delete,modify or search films and series
+					boolean endMenu = false;
+					System.out.println("You enter in the series and film menu, select one option:\n"
+							+ "1.-Add a new film or serie \n"
+							+ "2.-Delete a serie or film by his attributes \n"
+							+ "3.-Modify an attribute \n"
+							+ "4.-Search a film or serie \n"
+							+ "5.-Back");
+					option = scanner.next();
+					while(!endMenu)
+					{
+						switch(option)
+						{
+						case "1":
+							System.out.println("Choose which audiovisual do you want (serie\film)");
+							option = scanner.next();
+							if(option.equalsIgnoreCase("serie"))
+							{
+								System.out.println("You choose add a new serie please introduce his title");
+								this.title = scanner.next();
+								System.out.println("Introduce his original language");
+								this.originalLanguage = scanner.next();
+								System.out.println("Want to introduce adapted language? (yes/no)");
+								option = scanner.next();
+								while(!option.equalsIgnoreCase("yes") && !option.equalsIgnoreCase("no"))
+								{
+									System.out.println("Error introducing adapted language introduce yes or no again");
+									option = scanner.next();
+								}
+								if(option.equalsIgnoreCase("yes"))
+								{
+									while(!option.equalsIgnoreCase("no"))
+									{
+										System.out.println("Introduce a new language");
+										language = scanner.next();
+										adaptedLanguage = Arrays.copyOf(adaptedLanguage, adaptedLanguage.length+1);
+										adaptedLanguage[adaptedLanguage.length-1] = language;
+										System.out.println("Want to introduce a new language (no/any key)");
+										option=scanner.next();
+									}
+								}
+								//This try-catch it used if the user introduce a string data
+								try
+								{
+									System.out.println("Enter a duration");
+									duration = scanner.nextInt();
+								}catch(InputMismatchException exception)
+								{
+									System.out.println("Error introducing duration");
+									duration = 0;
+								}
+								//This try-catch have the same function of the last try-catch
+								try
+								{
+									System.out.println("Enter a view");
+									views = scanner.nextInt();
+								}catch(InputMismatchException exception)
+								{
+									System.out.println("Error introducing views");
+									views=0;
+								}
+								serie = menuFS.addSerie(serie, title, originalLanguage, adaptedLanguage, duration, views);
+							}
+							else if(option.equalsIgnoreCase("film"))
+							{
+								//The user must to introduce with the scanner the attributes of the films 
+								option = scanner.nextLine();
+								System.out.println("You choose add a new film please introduce his title");
+								this.title = scanner.nextLine();
+								System.out.println("Introduce his original language");
+								this.originalLanguage = scanner.next();
+								System.out.println("Want to introduce adapted language? (yes/no)");
+								option = scanner.next();
+								while(!option.equalsIgnoreCase("yes") && !option.equalsIgnoreCase("no"))
+								{
+									System.out.println("Error introducing adapted language introduce yes or no again");
+									option = scanner.next();
+								}
+								if(option.equalsIgnoreCase("yes"))
+								{
+									while(!option.equalsIgnoreCase("no"))
+									{
+										System.out.println("Introduce a new language");
+										language = scanner.next();
+										adaptedLanguage = Arrays.copyOf(adaptedLanguage, adaptedLanguage.length+1);
+										adaptedLanguage[adaptedLanguage.length-1] = language;
+										System.out.println("Want to introduce a new language (no/any key)");
+										option=scanner.next();
+									}
+								}
+								//This try catch it used if the user enter a String data
+								try
+								{
+									System.out.println("Enter a duration");
+									duration = scanner.nextInt();
+								}catch(InputMismatchException exception)
+								{
+									System.out.println("Error introducing duration");
+									duration = 0;
+								}
+								//This try-catch do the same function of the last try-catch
+								try
+								{
+									System.out.println("Enter a view");
+									views = scanner.nextInt();
+								}catch(InputMismatchException exception)
+								{
+									System.out.println("Error introducing views");
+									views=0;
+								}
+								film = menuFS.addFilm(film, title, originalLanguage, adaptedLanguage, duration, views);
+							}else
+								System.out.println("Error to selecting audiovisual");
+							break;
+						case "2":
+							System.out.println("Choose which audiovisual do you want (serie/film)");
+							option = scanner.next();
+							if(option.equalsIgnoreCase("serie"))
+							{
+								option = scanner.nextLine();
+								System.out.println("You choose delete a serie by his attribute select one of then: \n"
+										+ "1.-Title \n"
+										+ "2.-Original language \n"
+										+ "3.-Adapted language \n"
+										+ "4.-Duration \n"
+										+ "5.-Views");
+								option = scanner.nextLine();
+								if(option.equalsIgnoreCase("title") || option.equals("1"))
+								{
+									title = scanner.nextLine();
+									System.out.println("Select the title to search");
+									title = scanner.nextLine();
+									serie = menuFS.SeriesDropByTitle(serie, title);
+								}
+								else if(option.equalsIgnoreCase("original language") || option.equalsIgnoreCase("originalLanguage") || option.equals("2"))
+								{
+									originalLanguage = scanner.nextLine();
+									System.out.println("Select the original language to search");
+									originalLanguage = scanner.nextLine();
+									serie = menuFS.SeriesDropByOriginalLanguage(serie, originalLanguage);
+								}
+								else if(option.equalsIgnoreCase("adapted language") || option.equalsIgnoreCase("adaptedLanguage") || option.equals("3"))
+								{
+									adaptedLanguage = new String[0];
+									System.out.println("Select the adapted language to search (can be may several)");
+									option = scanner.next();
+									while(!option.equalsIgnoreCase("exit"))
+									{
+										adaptedLanguage = Arrays.copyOf(adaptedLanguage, adaptedLanguage.length+1);
+										adaptedLanguage[adaptedLanguage.length-1] = option;
+										System.out.println("Select another to add or \"exit\" to stop and search");
+										option = scanner.next();
+									}
+									serie = menuFS.SeriesDropByAdaptedLanguage(serie, adaptedLanguage);
+									
+								}
+								else if(option.equalsIgnoreCase("duration") || option.equals("4"))
+								{
+									
+									try
+									{
+										System.out.println("Enter a duration");
+										duration = scanner.nextInt();
+									}catch(InputMismatchException e)
+									{
+										System.out.println("Error to introduce the duration");
+										duration = -1;
+									}
+									if(duration==-1)
+										System.out.println("Error to delete the serie");
+									else
+										serie = menuFS.SeriesDownDuration(serie, duration);
+								}
+								else if(option.equalsIgnoreCase("views") || option.equals("5"))
+								{
+									System.out.println("Select the views to search");
+									try
+									{
+										System.out.println("Enter a view");
+										views = scanner.nextInt();
+									}catch(InputMismatchException e)
+									{
+										System.out.println("Error to introduce the views");
+										views = -1;
+									}
+									if(views<0)
+										System.out.println("Error to delete the serie");
+									else
+										serie = menuFS.SeriesDownViewTimesView(serie, views);
+											
+								}else
+									System.out.println("Error to introducing option");
+							}
+							else if(option.equalsIgnoreCase("film"))
+							{
+								option = scanner.nextLine();
+								System.out.println("You choose delete a film by his attribute select one of then: \n"
+										+ "1.-Title \n"
+										+ "2.-Original language \n"
+										+ "3.-Adapted language \n"
+										+ "4.-Duration \n"
+										+ "5.-Views");
+								option = scanner.nextLine();
+								if(option.equalsIgnoreCase("title") || option.equals("1"))
+								{
+									title = scanner.nextLine();
+									System.out.println("Select the title to search");
+									title = scanner.nextLine();
+									film = menuFS.removeFilmByTitle(film, title);
+								}
+								else if(option.equalsIgnoreCase("original language") || option.equalsIgnoreCase("originalLanguage") || option.equals("2"))
+								{
+									originalLanguage = scanner.nextLine();
+									System.out.println("Select the original language to search");
+									originalLanguage = scanner.nextLine();
+									film = menuFS.removeFilmByOriginalLanguage(film, originalLanguage);
+								}
+								else if(option.equalsIgnoreCase("adapted language") || option.equalsIgnoreCase("adaptedLanguage") || option.equals("3"))
+								{
+									adaptedLanguage = new String[0];
+									System.out.println("Select the adapted language to search (can be may several)");
+									option = scanner.next();
+									while(!option.equalsIgnoreCase("exit"))
+									{
+										adaptedLanguage = Arrays.copyOf(adaptedLanguage, adaptedLanguage.length+1);
+										adaptedLanguage[adaptedLanguage.length-1] = option;
+										System.out.println("Select another to add or \"exit\" to stop and search");
+										option = scanner.next();
+									}
+									film = menuFS.removeFilmByAdaptedLanguage(film, adaptedLanguage);
+								}
+								else if(option.equalsIgnoreCase("duration") || option.equals("4"))
+								{
+									System.out.println("Select the duration to search");
+									try
+									{
+										
+									}catch(InputMismatchException e)
+									{
+										System.out.println("Error to introduce the duration");
+										duration = -1;
+									}
+									if(duration==-1)
+										System.out.println("Error to delete the film");
+									else
+										film = menuFS.removeFilmByDuration(film, duration);
+								}
+								else if(option.equalsIgnoreCase("views") || option.equals("5"))
+								{
+									System.out.println("Select the views to search");
+									try
+									{
+										
+									}catch(InputMismatchException e)
+									{
+										System.out.println("Error to introduce the views");
+										views = -1;
+									}
+									if(views<0)
+										System.out.println("Error to delete the film");
+									else
+										film = menuFS.removeFilmByViews(film, views);
+											
+								}else
+								
+									System.out.println("Error to introducing option");
+							}
+						}
+						System.out.println("You enter in the series and film menu, select one option:\n"
+								+ "1.-Add a new film or serie \n"
+								+ "2.-Delete a serie or film by his attributes \n"
+								+ "3.-Modify an attribute \n"
+								+ "4.-Search a film or serie \n"
+								+ "5.-Back");
+						option = scanner.next();
+					}
 					
 					break;
 				case "2":
