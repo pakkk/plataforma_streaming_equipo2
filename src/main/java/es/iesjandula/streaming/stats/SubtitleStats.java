17665e26ba30610fc7b34a001b5cd90436f3a688
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-26 18:44:12 +0200

    Tarea021 Creada por Pablo Elias Ruiz Canovas

---
+++ src/main/java/es/iesjandula/streaming/stats/SubtitleStats.java
@@ -0,0 +1,95 @@
+package es.iesjandula.streaming.stats;
+import java.util.Arrays;
+import es.iesjandula.streaming.streaming.*;
+import es.iesjandula.streaming.baseclasses.Subtitles;
+public class SubtitleStats {
+	//This class takes care of show information of the series and film that have several subtitles
+	
+	//This method takes care of show how many subtitles there are in the series
+	public void countSubtitlesSerie (Serie arraySerie[])
+	{
+		int sum = 0;
+		Subtitles subtitle[];
+		for(int i = 0;i<arraySerie.length;i++)
+		{
+			subtitle = arraySerie[i].getSubtitles();
+			sum+=subtitle.length;
+		}
+		System.out.println("There are "+sum+" subtitles in the serie");
+	}
+	//This method takes care of show how many subtitles there are in the films
+	public void countSubtitlesFilm (Film arrayFilm[])
+	{
+		int sum = 0;
+		Subtitles subtitle[];
+		for(int i = 0;i<arrayFilm.length;i++)
+		{
+			subtitle = arrayFilm[i].getSubtitles();
+			sum+=subtitle.length;
+		}
+		System.out.println("There are "+sum+" subtitles in the film");
+	}
+	//This method takes care of show how many subtitles there are in our systems
+	public void countAllSubtitles(Serie arraySerie[],Film arrayFilm[])
+	{
+		int sum = 0;
+		Subtitles subtitle[];
+		for(int i = 0;i<arraySerie.length;i++)
+		{
+			subtitle = arraySerie[i].getSubtitles();
+			sum+=subtitle.length;
+		}
+		
+		for(int i = 0;i<arrayFilm.length;i++)
+		{
+			subtitle = arrayFilm[i].getSubtitles();
+			sum+=subtitle.length;
+		}
+		System.out.println("There are "+sum+" subtitles in our systems");
+	}
+	//This method takes care of show how many subtitles there are in the serie 
+	public void showSubtitlesSerie(Serie arraySerie[])
+	{
+		Subtitles subtitle[];
+		for(Serie serie:arraySerie)
+		{
+			subtitle = serie.getSubtitles();
+			System.out.println(Arrays.toString(subtitle));
+		}
+	}
+	//This method takes care of show how many subtitles there are in the serie 
+	public void showSubtitlesFilm(Film arrayFilm[])
+	{
+		Subtitles subtitle[];
+		for(Film film:arrayFilm)
+		{
+			subtitle = film.getSubtitles();
+			System.out.println(Arrays.toString(subtitle));
+		}
+	}
+	//This method show the films that have 2 or more subtitles
+	public void infoFilmSubtitles(Film arrayFilm[])
+	{
+		Subtitles subtitle[];
+		for(Film film:arrayFilm)
+		{
+			subtitle = film.getSubtitles();
+			if(subtitle.length>=2)
+				System.out.println(Arrays.toString(subtitle));
+		}
+	}
+	//This method show the films that have 2 or more subtitles
+	public void infoSerieSubtitles(Serie arraySerie[])
+	{
+		Subtitles subtitle[];
+		for(Serie serie:arraySerie)
+		{
+			subtitle = serie.getSubtitles();
+			if(subtitle.length>=2)
+				System.out.println(Arrays.toString(subtitle));
+		}
+	}
+	
+	
+	
+}
