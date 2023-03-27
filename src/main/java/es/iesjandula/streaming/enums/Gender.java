ba3e9306a47c4e66724dd9cb43d75f17c83f28fa
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-27 14:41:10 +0200

    Ajuste de enums

--- src/main/java/es/iesjandula/streaming/enums/Gender.java
+++ src/main/java/es/iesjandula/streaming/enums/Gender.java
@@ -1,5 +1,6 @@
 package es.iesjandula.streaming.enums;
 
 public enum Gender {
+	//Enum that saves the gender of a actor or director 
 	HOMBRE,MUJER,OTRO;
 }
--- src/main/java/es/iesjandula/streaming/enums/RatingType.java
+++ src/main/java/es/iesjandula/streaming/enums/RatingType.java
@@ -1,5 +1,6 @@
 package es.iesjandula.streaming.enums;
 
 public enum RatingType {
+	//Enum that save the rating of a recommendation of a serie or film
 	POSITIVE,NEGATIVE;
 }
