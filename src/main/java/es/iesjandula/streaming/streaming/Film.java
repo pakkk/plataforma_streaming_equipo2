2cdf9c20f805e20e547c71b4ea9952bda45c629f
Alejandro Cazalla Pérez <acazper592@h.educaand.es>
2023-03-28 17:38:38 +0200

    Clases Film y Serie comentarios modificados

--- src/main/java/es/iesjandula/streaming/streaming/Film.java
+++ src/main/java/es/iesjandula/streaming/streaming/Film.java
@@ -1,14 +1,16 @@
 package es.iesjandula.streaming.streaming;
 
 public class Film extends Audiovisual {
-    //Class that stores audiovisual data after loading it into an array
+    /**Class that stores audiovisual data after loading it into an array*/
 
-    //Builder
-    //This builder comes from the Audiovisual class  
+    /**
+    * Builder
+    * This builder comes from the Audiovisual class  
+    */
     public Film(String title, String originalLanguage,String adaptedLanguage[], int duration, int views)
     {
         super(title, originalLanguage,adaptedLanguage, duration, views);
 
     }
-    //The methods are inherited from the Audiovisual class so it is not necessary to introduce them here	
+    /**The methods are inherited from the Audiovisual class so it is not necessary to introduce them here*/	
 }
--- src/main/java/es/iesjandula/streaming/streaming/Serie.java
+++ src/main/java/es/iesjandula/streaming/streaming/Serie.java
@@ -1,14 +1,16 @@
 package es.iesjandula.streaming.streaming;
 
 public class Serie extends Audiovisual{
-	//This Class takes care of save data of audiovisual to load the data in one array 
+	/**This Class takes care of save data of audiovisual to load the data in one array*/
 	
-	//Constructor
-	//This constructor comes from the audiovisual class
+	/**
+        * Constructor
+	* This constructor comes from the audiovisual class
+        */
 	public Serie(String title, String originalLanguage,String adaptedLanguage[], int duration, int views) 
 	{
 		super(title, originalLanguage,adaptedLanguage, duration, views);
 	}
-	//Methods extends of audiovisual class, so no need to enter here
+	/**Methods extends of audiovisual class, so no need to enter here*/
 	
 }
