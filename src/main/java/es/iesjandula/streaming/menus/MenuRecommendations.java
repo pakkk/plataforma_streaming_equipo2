c925bc8e00aa9b8e3bee12e9929913e0dde87bb7
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-03-28 12:21:39 +0200

    Tarea011 Ajuste de comentarios y nombre de los metodos

--- src/main/java/es/iesjandula/streaming/menus/MenuRecommendations.java
+++ src/main/java/es/iesjandula/streaming/menus/MenuRecommendations.java
@@ -6,21 +6,22 @@ import es.iesjandula.streaming.baseclasses.Recommendation;
 import es.iesjandula.streaming.enums.RatingType;
 
 public class MenuRecommendations {
-	// Class that manages the methods for adding, deleting, querying, and searching Recommendations
+	//Class that manages the methods for adding, deleting, querying, and searching Recommendations
 
-	public Recommendation[] RecommendationPeliculasAltas(Recommendation array[], RatingType rate, String comment) 
+	public Recommendation[] recommendationPeliculasAltas(Recommendation array[], RatingType rate, String comment) 
 	{
-		// This method adds a recommendation for a movie or TV show, passing its attributes as parameters, and returns the modified array
+		//This method adds a recommendation for a movie or TV show, passing its attributes as parameters, and returns the modified array
 		array = Arrays.copyOf(array, array.length + 1);
 		array[array.length - 1] = new Recommendation(rate, comment);
 		return array;
 	}
 
-	public Recommendation[] RecommendationDeleteByComment(Recommendation array[], String comment) 
+	public Recommendation[] recommendationDeleteByComment(Recommendation array[], String comment) 
 	{
-		// This method deletes a recommendation for a movie or TV show by searching for a specific comment and removing that recommendation
-		// The method receives the comment that will delete the recommendation as a parameter, as well as the array of recommendations to modify, and returns the modified array
-		// This for loop searches the array of recommendations for recommendations that contain the same comment to delete it
+		/**This method deletes a recommendation for a movie or TV show by searching for a specific comment and removing that recommendation
+		 * The method receives the comment that will delete the recommendation as a parameter, as well as the array of recommendations to modify, and returns the modified array
+		 * This for loop searches the array of recommendations for recommendations that contain the same comment to delete it
+		 */
 		for (int i = 0; i < array.length; i++) 
 		{
 			if (comment.equalsIgnoreCase(((Recommendation) array[i]).getComment())) 
@@ -33,11 +34,12 @@ public class MenuRecommendations {
 		return array;
 	}
 
-	public Recommendation[] RecommendationBajasPorrate(Recommendation array[], RatingType rate)
+	public Recommendation[] recommendationBajasPorrate(Recommendation array[], RatingType rate)
 	{
-		// This method deletes a recommendation for a movie or TV show by searching for a specific rating and removing that recommendation
-		// The method receives the rating that will delete the recommendation as a parameter, as well as the array of recommendations to modify, and returns the modified array
-		// This for loop searches the array of recommendations for recommendations that contain the same rating to delete it
+		/** This method deletes a recommendation for a movie or TV show by searching for a specific rating and removing that recommendation
+		 * The method receives the rating that will delete the recommendation as a parameter, as well as the array of recommendations to modify, and returns the modified array
+		 * This for loop searches the array of recommendations for recommendations that contain the same rating to delete it
+		 */
 		for (int i = 0; i < array.length; i++)
 		{
 			if (rate.equals(((Recommendation) array[i]).getRating())) 
@@ -49,11 +51,12 @@ public class MenuRecommendations {
 		}
 		return array;
 	}
-
-	public Recommendation[] RecommendationModificacionPorcomment(Recommendation array[], String comment, String arraycomment) 
+	
+	public Recommendation[] recommendationModificacionPorcomment(Recommendation array[], String comment, String arraycomment) 
 	{
-		//This method modify a comment for a recommendations for a movie or serie by searching for a specific rating and removing that recommendation
-		//This loop iterates through the recommendations array and searches for recommendations that contain the old comment to replace it with the new one.
+		/**This method modify a comment for a recommendations for a movie or serie by searching for a specific rating and removing that recommendation
+		 *This loop iterates through the recommendations array and searches for recommendations that contain the old comment to replace it with the new one.
+		 */
 		for (int i = 0; i < array.length; i++) 
 		{
 			if (comment.equalsIgnoreCase(((Recommendation) array[i]).getComment())) 
@@ -68,7 +71,7 @@ public class MenuRecommendations {
 
 	//This method is responsible for modifying the "rate" attribute to a new one. It takes as parameters the recommendations array, the old rating, and the new rating to replace it.
 	//It returns the modified array.
-	public Recommendation[] RecommendationesModificacionPorrate(Recommendation array[], RatingType rate, RatingType nuevarate) 
+	public Recommendation[] recommendationesModificacionPorrate(Recommendation array[], RatingType rate, RatingType nuevarate) 
 	{
 		//This loop iterates through the recommendations array and searches for recommendations that contain the old rating to replace it with the new one.
 		for (int i = 0; i < array.length; i++) 
@@ -84,7 +87,7 @@ public class MenuRecommendations {
 	}
 
 	//This method is responsible for searching for recommendations by their comments. It takes as parameters the recommendations array and the comment to search for.
-	public void RecommendationesBusquedaPorcomment(Recommendation array[], String comment)
+	public void recommendationesBusquedaPorcomment(Recommendation array[], String comment)
 	{
 		//This loop iterates through the recommendations array and searches for recommendations that contain the comment to search for. If it is not found, the user will be notified.
 		boolean encontrado = false;
@@ -101,7 +104,7 @@ public class MenuRecommendations {
 	}
 
 	//This method is responsible for searching for recommendations by their rating. It takes as parameters the recommendations array and the rating to search for.
-	public void RecommendationesBusquedaPorrate(Recommendation array[], RatingType rate) 
+	public void recommendationesBusquedaPorrate(Recommendation array[], RatingType rate) 
 	{
 		//This loop iterates through the recommendations array and searches for recommendations that contain the rating to search for. If it is not found, the user will be notified.
 		boolean encontrado = false;
