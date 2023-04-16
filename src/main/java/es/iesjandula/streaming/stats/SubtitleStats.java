351ca7d152eab8cc43d3ae142d936c75abf1f2a3
Alvarolopmon <alopmon563@g.educaand.es>
2023-04-16 13:23:20 +0200

    Refactor clases by Alvaro Lopez Montero

--- src/main/java/es/iesjandula/streaming/stats/SubtitleStats.java
+++ src/main/java/es/iesjandula/streaming/stats/SubtitleStats.java
@@ -1,14 +1,21 @@
 package es.iesjandula.streaming.stats;
-import java.util.Arrays;
+
+import java.util.Arrays; 
 import es.iesjandula.streaming.streaming.*;
 import es.iesjandula.streaming.baseclasses.Subtitles;
-public class SubtitleStats {
-    /**This class takes care of show information of the series and film that have several subtitles*/
 
+/**
+ * This class takes care of show information of the series and film that have several subtitles
+ * @author Pablo
+ */
+
+public class SubtitleStats {
+  
     /**
      * This method takes care of show how many subtitles there are in the series
      * @param arraySerie 
      */
+	
     public void countSubtitlesSerie (Serie arraySerie[])
     {
         int sum = 0;
@@ -23,8 +30,9 @@ public class SubtitleStats {
     
     /**
      * This method takes care of show how many subtitles there are in the films
-     * @param arrayFilm 
+     * @param arrayFilm array with the films 
      */
+    
     public void countSubtitlesFilm (Film arrayFilm[])
     {
         int sum = 0;
@@ -39,9 +47,10 @@ public class SubtitleStats {
     
     /**
      * This method takes care of show how many subtitles there are in our systems
-     * @param arraySerie
-     * @param arrayFilm 
+     * @param arraySerie array with the series
+     * @param arrayFilm array with the films 
      */
+    
     public void countAllSubtitles(Serie arraySerie[],Film arrayFilm[])
     {
         int sum = 0;
@@ -62,8 +71,9 @@ public class SubtitleStats {
      
     /**
      * This method takes care of show how many subtitles there are in the serie
-     * @param arraySerie 
+     * @param arraySerie array with the series
      */
+    
     public void showSubtitlesSerie(Serie arraySerie[])
     {
         Subtitles subtitle[];
@@ -76,8 +86,9 @@ public class SubtitleStats {
  
     /**
      * This method takes care of show how many subtitles there are in the serie
-     * @param arrayFilm 
+     * @param arrayFilm array with the films 
      */
+    
     public void showSubtitlesFilm(Film arrayFilm[])
     {
 	    Subtitles subtitle[];
@@ -90,8 +101,9 @@ public class SubtitleStats {
     
     /**
      * This method show the films that have 2 or more subtitles
-     * @param arrayFilm 
+     * @param arrayFilm array with the films
      */
+    
     public void infoFilmSubtitles(Film arrayFilm[])
     {
         Subtitles subtitle[];
@@ -107,8 +119,9 @@ public class SubtitleStats {
     
     /**
      * This method show the films that have 2 or more subtitles
-     * @param arraySerie 
+     * @param arraySerie array with the series
      */
+    
     public void infoSerieSubtitles(Serie arraySerie[])
     {
         Subtitles subtitle[];
@@ -121,7 +134,4 @@ public class SubtitleStats {
             }
         }
     }
-	
-	
-	
 }
