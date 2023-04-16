4d164ad55f6794d653ea78c6501e77679b714f04
Alvarolopmon <alopmon563@g.educaand.es>
2023-04-16 13:29:35 +0200

    Refactor comment by Alvaro Lopez Montero

--- src/main/java/es/iesjandula/streaming/streaming/Serie.java
+++ src/main/java/es/iesjandula/streaming/streaming/Serie.java
@@ -8,15 +8,15 @@ package es.iesjandula.streaming.streaming;
 public class Serie extends Audiovisual{
 	
 	/**
-	* Parametriced constructor.
-	* Constructor comes from the Audiovisual class
-    * @param title the title of the Audiovisual
-    * @param originalLanguage the original language of the Audiovisual
-    * @param adaptedLanguages the adapted languages of the Audiovisual
-	* @param duration the duration of the Audiovisual
-	* @param views the views of the Audiovisual   
-    */
-	
+	 * Parametriced constructor.
+	 * Constructor comes from the Audiovisual class
+	 * @param title the title of the Audiovisual
+	 * @param originalLanguage the original language of the Audiovisual
+	 * @param adaptedLanguages the adapted languages of the Audiovisual
+	 * @param duration the duration of the Audiovisual
+	 * @param views the views of the Audiovisual
+	 * */
+		
 	public Serie(String title, String originalLanguage,String adaptedLanguage[], int duration, int views) 
 	{
 		super(title, originalLanguage,adaptedLanguage, duration, views);
